4f05f783141d1ef92b5237018412b95218c5f3f8
Lorenzo Teodorani <l.teodorani@gmail.com>
2016-04-26 13:53:52 +0200

    Aggiunta classe per l'implementazione del sensore MPL

---
+++ Sorgenti/GorDevices/Pressure_MPL3115A2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Gor.Devices
+{
+    class Pressure_MPL3115A2 : Sensor
+    {
+        Process p;
+        public Pressure_MPL3115A2(string name, string externalCommand, Logger logger ) : base(name, false, logger)
+        {
+            p = new Process();
+            p.StartInfo.FileName = externalCommand;
+            p.StartInfo.Arguments = "";
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            
+        }
+        public string Read()
+        {
+            p.Start();
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            return output;
+        }
+    }
+}
---
+++ Sorgenti/Tests/Test.Shell.Pressure/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gor.Devices;
+
+namespace Test.Shell.Pressure
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
